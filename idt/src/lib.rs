9738c8ef94e33767303875e933252eda98d7c561
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-04 11:02:01 +0000

    refactor(idt): rewrite IDT from the ground up using lazy_static
    - Replace manual unsafe static mut with lazy_static for safer IDT management
    - Remove all unsafe IDT initialization code and encapsulate in safe abstraction
    - Improve code clarity, safety, and maintainability for IDT setup
    - Prepare IDT for flexible, modular interrupt handler

--- idt/src/lib.rs
+++ idt/src/lib.rs
@@ -1,29 +1,51 @@
 #![feature(abi_x86_interrupt)]
 #![no_std]
 
-use core::ptr::addr_of_mut;
 use hal::cpu;
+use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
-extern "x86-interrupt" fn divide_by_zero(_stack_frame: InterruptStackFrame) {
-	cpu::halt();
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.divide_error.set_handler_fn(divide_by_zero_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
+        idt
+    };
+}
+extern "x86-interrupt" fn divide_by_zero_handler(_stack: InterruptStackFrame) {
+    //TODO: Log/panic as needed
+    loop {
+        cpu::halt();
+    }
 }
 
-extern "x86-interrupt" fn page_fault(_stack_frame: InterruptStackFrame, _err: PageFaultErrorCode) {
-	cpu::halt();
+extern "x86-interrupt" fn page_fault_handler(
+    _stack: InterruptStackFrame,
+    _err: PageFaultErrorCode,
+) {
+    //TODO: Log info, show faulting address
+    loop {
+        cpu::halt();
+    }
 }
 
-extern "x86-interrupt" fn double_fault(_stack_frame: InterruptStackFrame, _err: u64) -> ! {
-	loop {}
+extern "x86-interrupt" fn double_fault_handler(_stack: InterruptStackFrame, _err: u64) -> ! {
+    //TODO: Log and maybe show stack frame
+    loop {
+        cpu::halt();
+    }
 }
 
 pub fn init_idt() {
-	unsafe {
-		let idt_ptr: *mut InterruptDescriptorTable = addr_of_mut!(IDT);
-		(*idt_ptr).divide_error.set_handler_fn(divide_by_zero);
-		(*idt_ptr).page_fault.set_handler_fn(page_fault);
-		(*idt_ptr).double_fault.set_handler_fn(double_fault);
-		(*idt_ptr).load();
-	}
+    IDT.load();
+}
+
+pub unsafe fn register_interrupt_handler(
+    idt: &mut InterruptDescriptorTable,
+    vector: u8,
+    handler: extern "x86-interrupt" fn(InterruptStackFrame),
+) {
+    idt[vector].set_handler_fn(handler);
 }
