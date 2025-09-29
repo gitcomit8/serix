f550293433881b1bae0bf98a1751e4e4a1acd089
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-29 18:34:07 +0530

    feat(idt): complete exception handlers with detailed diagnostics
    - Implement divide-by-zero, page-fault, double-fault handlers
    by invoking util crate's panic/oops routines
    
    - Added serial console diagnostics including faulting instruction
    pointers and CR2 register reading for page faults

--- idt/src/lib.rs
+++ idt/src/lib.rs
@@ -1,51 +1,56 @@
 #![feature(abi_x86_interrupt)]
 #![no_std]
 
-use hal::cpu;
+use hal::serial_println;
 use lazy_static::lazy_static;
+use util::panic::oops;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 lazy_static! {
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.divide_error.set_handler_fn(divide_by_zero_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
-        idt
-    };
+	static ref IDT: InterruptDescriptorTable = {
+		let mut idt = InterruptDescriptorTable::new();
+		idt.divide_error.set_handler_fn(divide_by_zero_handler);
+		idt.page_fault.set_handler_fn(page_fault_handler);
+		idt.double_fault.set_handler_fn(double_fault_handler);
+		idt
+	};
 }
 extern "x86-interrupt" fn divide_by_zero_handler(_stack: InterruptStackFrame) {
-    //TODO: Log/panic as needed
-    loop {
-        cpu::halt();
-    }
+	oops("Divide by Zero exception");
 }
 
-extern "x86-interrupt" fn page_fault_handler(
-    _stack: InterruptStackFrame,
-    _err: PageFaultErrorCode,
-) {
-    //TODO: Log info, show faulting address
-    loop {
-        cpu::halt();
-    }
+extern "x86-interrupt" fn page_fault_handler(stack: InterruptStackFrame, err: PageFaultErrorCode) {
+	serial_println!(
+		"Page fault at instruction pointer: {:#x}",
+		stack.instruction_pointer.as_u64()
+	);
+
+	let cr2: u64;
+	unsafe {
+		core::arch::asm!("mov {}, cr2", out(reg) cr2);
+	}
+	serial_println!("Page fault address: {:#x}", cr2);
+	serial_println!("Error Code: {:?}", err);
+
+	oops("Page fault exception");
 }
 
 extern "x86-interrupt" fn double_fault_handler(_stack: InterruptStackFrame, _err: u64) -> ! {
-    //TODO: Log and maybe show stack frame
-    loop {
-        cpu::halt();
-    }
+	serial_println!(
+		"Double fault at instruction pointer: {:#x}",
+		_stack.instruction_pointer.as_u64()
+	);
+	panic!("Double fault exception");
 }
 
 pub fn init_idt() {
-    IDT.load();
+	IDT.load();
 }
 
 pub unsafe fn register_interrupt_handler(
-    idt: &mut InterruptDescriptorTable,
-    vector: u8,
-    handler: extern "x86-interrupt" fn(InterruptStackFrame),
+	idt: &mut InterruptDescriptorTable,
+	vector: u8,
+	handler: extern "x86-interrupt" fn(InterruptStackFrame),
 ) {
-    idt[vector].set_handler_fn(handler);
+	idt[vector].set_handler_fn(handler);
 }
