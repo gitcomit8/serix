6762d6bd24a36bd00a03c2017c240c203b0fab3a
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-30 11:22:41 +0530

    refactor: reindent files with tab

--- hal/src/lib.rs
+++ hal/src/lib.rs
@@ -4,23 +4,4 @@
 pub mod io;
 pub mod serial;
 pub use io::*;
-pub use serial::{init_serial,serial_print};
-
-pub mod cpu {
-    use x86_64::instructions::*;
-
-    #[inline(always)]
-	pub fn halt() {
-		hlt();
-	}
-
-	#[inline(always)]
-	pub fn enable_interrupts() {
-		interrupts::enable();
-	}
-
-	#[inline(always)]
-	pub fn disable_interrupts() {
-		interrupts::disable();
-	}
-}
+pub use serial::{init_serial, serial_print};
